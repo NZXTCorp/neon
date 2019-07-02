17aca28bf7e168c70dc7e55fa17f579a251a552e
Ruwen Hahn <ruwen.hahn@nzxt.com>
2019-07-02 19:21:31 +0200

    Ignore delay load if current file is not a node module
    This prevents breaking tests in the node module that create
    native executables which don't load node

--- crates/neon-runtime/src/win_delay_load_hook.cc
+++ crates/neon-runtime/src/win_delay_load_hook.cc
@@ -13,12 +13,39 @@
 #include <delayimp.h>
 #include <string.h>
 
+static bool module_name_checked = false;
+static bool ignore_delay_load = false;
+
+static bool check_module_name() {
+  const DWORD len = 512;
+  wchar_t module_name[len] = {0};
+  auto actual_len = GetModuleFileNameW(nullptr, module_name, len);
+  if (len == actual_len) {
+    return false;
+  }
+
+  wchar_t drive[_MAX_DRIVE] = {0};
+  wchar_t dir[_MAX_DIR] = {0};
+  wchar_t fname[_MAX_FNAME] = {0};
+  wchar_t ext[_MAX_EXT] = {0};
+  if (_wsplitpath_s(module_name, drive, _MAX_DRIVE, dir, _MAX_DIR, fname, _MAX_FNAME, ext, _MAX_EXT) != 0) {
+    return false;
+  }
+
+  return _wcsicmp(ext, L"node") == 0;
+}
+
 FARPROC WINAPI load_exe_hook(unsigned int event, DelayLoadInfo* info) {
   HMODULE m;
   if (event != dliNotePreLoadLibrary)
     return NULL;
 
-  if (_stricmp(info->szDll, "node.exe") != 0)
+  if (!module_name_checked) {
+    ignore_delay_load = check_module_name();
+    module_name_checked = true;
+  }
+
+  if (!ignore_delay_load && stricmp(info->szDll, "node.exe") != 0)
     return NULL;
 
 	
